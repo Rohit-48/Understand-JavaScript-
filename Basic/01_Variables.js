95bdf9010fcd854c8da15f8d7553a0c314893d0a
Rohit Mandavkar <97210279+Rohit-48@users.noreply.github.com>
2023-10-16 05:48:19 +0000

    Code and Addes New File

---
+++ Basic/01_Variables.js
@@ -0,0 +1,13 @@
+const accountId = 12462
+let accountEmail = "rohitmandavkar3577@gmail.com"
+var accountPassword = "12345"
+accountCity = "Chiplun"
+let accountState;
+
+// accountId = 2
+console.table([accountId, accountEmail, accountPassword,accountCity, accountState])
+
+/*
+Prefer not to used Var 
+because  of issue in block Scope and Functional Scope 
+*/
