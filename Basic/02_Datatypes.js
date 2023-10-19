65bc91564bfda5d4ef9201ea56e1f9988c8e8807
Rohit Mandavkar <97210279+Rohit-48@users.noreply.github.com>
2023-10-19 07:33:01 +0000

    Data Types Notes and Learning

--- Basic/02_Datatypes.js
+++ Basic/02_Datatypes.js
@@ -2,3 +2,24 @@
 
 //alert(3+3)  // we are using Node.js not Borswer
 
+console.log(2+2); 
+console.log("Professor")
+
+
+let name = "Rohit"
+let age = 18
+let isLoggedIn = false
+
+// number => 2to power 53
+// bigint 
+// string =>
+// boolean = true/false
+// null => standalone value
+// undefined => 
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof null);
+
