f191010e7983f371ec9cc7368674f2fcf827b268
Rohit Mandavkar <97210279+Rohit-48@users.noreply.github.com>
2023-11-03 10:02:21 +0000

    Math and Num

---
+++ 01_Basic/06_Num_Math.js
@@ -0,0 +1,39 @@
+const score = 400
+
+const balance = new Number(100)
+
+// console.log(balance);
+
+
+// console.log(balance.toString())
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1))
+
+const otherNumber = 1123.789
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 100000000
+
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//+++++++++++++++++++++++++++++++++ Math ++++++++++++++++++++++++++++++++++++++++++++++++//
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.2));
+// console.log(Math.ceil(6.1));
+// console.log(Math.floor(6.9));
+
+
+
+console.log(Math.random());
+console.log((Math.random()*10) +1 );
+console.log((Math.random()*10) +1 );
+
+const min = 200
+const max = 300
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
