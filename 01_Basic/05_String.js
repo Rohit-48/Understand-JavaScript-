bc68ebbae9f6d1aeec217387700abeb58304ed94
Rohit Mandavkar <97210279+Rohit-48@users.noreply.github.com>
2023-11-03 05:40:21 +0000

    String

--- 01_Basic/05_String.js
+++ 01_Basic/05_String.js
@@ -32,4 +32,17 @@ const url = "https://boreing.com/elonmusk%20rtyuio"
 
 // console.log(url.replace('%20', '-'));
 
-console.log(gameName.split('-'));
+// console.log(gameName.split('-'));
+
+
+const strPrim = "foo"; // A literal is a string primitive
+const strPrim2 = String(1); // Coerced into the string primitive "1"
+const strPrim3 = String(true); // Coerced into the string primitive "true"
+const strObj = new String(strPrim); // String with new returns a string wrapper object.
+
+// console.log(typeof strPrim); // "string"
+// console.log(typeof strPrim2); // "string"
+// console.log(typeof strPrim3); // "string"
+// console.log(typeof strObj); // "object"
+
+
