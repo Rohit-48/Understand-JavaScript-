97a149a74996f1e730b4e49deaf447ce578edb0d
Rohit Mandavkar <97210279+Rohit-48@users.noreply.github.com>
2023-11-03 10:03:27 +0000

    Edited Math and Num File

--- 01_Basic/06_Num_Math.js
+++ 01_Basic/06_Num_Math.js
@@ -29,11 +29,11 @@ const hundreds = 100000000
 
 
 
-console.log(Math.random());
-console.log((Math.random()*10) +1 );
-console.log((Math.random()*10) +1 );
+// console.log(Math.random());
+// console.log((Math.random()*10) +1 );
+// console.log((Math.random()*10) +1 );
 
 const min = 200
 const max = 300
 
-console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min)
