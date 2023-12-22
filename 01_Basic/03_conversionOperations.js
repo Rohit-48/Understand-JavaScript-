2572725a2f067375f247d51e2811105ebe5cf129
Rohit Mandavkar <97210279+Rohit-48@users.noreply.github.com>
2023-12-22 07:36:14 +0000

    Revision of Conversion Operation In JavaScript

--- 01_Basic/03_conversionOperations.js
+++ 01_Basic/03_conversionOperations.js
@@ -1,94 +1,83 @@
-let score = true
+let score = "Rohit"
 
-console.log(typeof score);
-console.log(typeof (score));
-
+//console.log(typeof score);
+//console.log(typeof(score));
 
 let valueInNumber = Number(score)
 // console.log(typeof valueInNumber);
 // console.log(valueInNumber);
 
+
 // "33" => 33
 // "33abc" => NaN
-// true => 1, false => 0
+//  true => 1; false => 0
 
-let isLoggedIn = ""
+let isLoggedIn = "Rohit"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-// console.log(booleanIsLoggedIn) 
+//  console.log(booleanIsLoggedIn);
 
-
-// 1 => true , 0 => false
+// 1 => true; 0 => false
 // "" => false
-// "Rohit" => true
-
+// "hitesh" => true
 
 let someNumber = 33
+
 let stringNumber = String(someNumber)
-// console.log(stringNumber)
-// console.log(typeof stringNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
-// ********************************Operations**********************************************
-
+// *********************** Operations ***********************
 
 let value = 3
 let negValue = -value
-// console.log(negValue)
+// console.log(negValue);
 
-let str1 =  "Hello"
-let str2 = " Rohit"
-// console.log(str1 + str2)
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Ronie"
+
+let str3 = str1 + str2
+// console.log(str3);
 
 // console.log("1" + 2);
 // console.log(1 + "2");
 // console.log("1" + 2 + 2);
 // console.log(1 + 2 + "2");
 
-// console.log(true)
+// console.log( (3 + 4) * 5 % 3);
 
+// console.log(+true);
+// console.log(+"");
 
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
 
 let gameCounter = 100
 ++gameCounter;
 // console.log(gameCounter);
 
-// Postfix increment: The operand is incremented after the expression is evaluated.
+/* JavaScript Demo: Expressions - Increment operator */
+
 let x = 3;
 const y = x++;
 
-// console.log(`x:${x}, y:${y}`);
+console.log(`x:${x}, y:${y}`);
 // Expected output: "x:4, y:3"
 
-// Prefix increment: The operand is incremented before the expression is evaluated.
 let a = 3;
 const b = ++a;
 
-// console.log(`a:${a}, b:${b}`);
+console.log(`a:${a}, b:${b}`);
 // Expected output: "a:4, b:4"
 
-// Example Of  Postfix
-let n = 3;
-const m = n++;
-// x is 4; y is 3
 
-let x2 = 3n;
-const y2 = x2++;
-// x2 is 4n; y2 is 3n
-
-// console.log(`n:${n}, m:${m}`);
-/* The meaning of the writing `n:{n}is to print the value of the variablento the console with the labeln:`. This is useful for debugging and logging purposes.
-The meaning of the give (             ) code is to print a space to the console. This is useful for formatting the output of the console.log() statement. */
-
-
-// Example Of Prefix:
-let c = 3;
-const d = ++x;
-// x is 4; y is 4
-
-let c2 = 3n;
-const d2 = ++x2;
-// x2 is 4n; y2 is 4n
-
-console.log(`c:${c}, d:${d}`);
-console.log(`c2:${c2}, d2:${d2}`);
-
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
